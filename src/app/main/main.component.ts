1d16d77d97543b1e6dad3b1243f9f4adb7a2abdd
jakobr0cky <jakob.rcknbr@gmail.com>
2024-02-09 02:40:25 +0100

    initial commit

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { default as proj4 } from 'proj4';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent {
+  options: string[] = [];
+  googleMapsLink:string = '';
+
+  async updateDropdown(event: Event) {
+    var element = event.target as HTMLInputElement;
+
+    var response = await fetch(`https://data.wien.gv.at/daten/OGDAddressService.svc/GetAddressInfo?Address=${element.value}&limit=10`);
+
+    if (!response || !response.ok) {
+      console.error('Failed to fetch data');
+      return;
+    }
+
+    try {
+      const responseData = await response.json();
+      const temp = [];
+
+      for (let i = 0; i < Math.min(responseData.features.length, 20); i++) {
+        temp.push(responseData.features[i].properties.Adresse);
+      }
+
+      this.options = temp;
+    } catch (error) {
+      console.error('Error processing response:', error);
+    }
+  }
+
+  createGoogleMapsLink(event: Event) {
+    var element = event.target as HTMLInputElement;
+    
+    this.getCoordinates(element.value).then((result)=>{
+      var location;
+      if (!("geolocation" in navigator)) {
+        location = 'standard location'
+      } 
+      navigator.geolocation.getCurrentPosition((position) => {
+        const currentLocation = position.coords.latitude+','+position.coords.longitude;
+
+        this.googleMapsLink = `https://www.google.com/maps/dir/${currentLocation}/${result}`;
+    });
+    });
+  }
+
+  async getCoordinates(address:string) {
+    try {
+        const response = await fetch(`https://data.wien.gv.at/daten/OGDAddressService.svc/GetAddressInfo?Address=${address}`);
+        const responseData = await response.json();
+  
+        proj4.defs("EPSG:31256", "+proj=tmerc +lat_0=0 +lon_0=16.33333333333333 +k=1 +x_0=0 +y_0=-5000000 +ellps=bessel +towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232 +units=m +no_defs");
+        proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
+  
+        const coordinates4326 = proj4("EPSG:31256", "EPSG:4326", responseData.features[0].geometry.coordinates);
+  
+        return coordinates4326[1] + ',' + coordinates4326[0];
+    } catch (error) {
+        console.log('error: '+error);
+        return;
+    }
+  }
+}
---
+++ temp/server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const proj4 = require('proj4');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(express.static('./'));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/example', (req, res) => {
+  console.log('bodyyyyyyy'+JSON.stringify(req.body));
+
+  getCoordinates(req.body.adress).then(coordinates =>{
+    res.status(200).json({ message: 'POST request received successfully','coordinates':coordinates });
+    return;
+  })
+
+  // Send a response (you can customize the response as needed)
+  //res.status(404).json({ message: 'error'});
+});
+
+async function getCoordinates(address) {
+  try {
+      const req = `https://data.wien.gv.at/daten/OGDAddressService.svc/GetAddressInfo?Address=${address}`;
+      console.log(req);
+      const response = await fetch(req);
+      const responseData = await response.json();
+      console.log(responseData.features[0].geometry.coordinates);
+
+      proj4.defs("EPSG:31256", "+proj=tmerc +lat_0=0 +lon_0=16.33333333333333 +k=1 +x_0=0 +y_0=-5000000 +ellps=bessel +towgs84=577.326,90.129,463.919,5.137,1.474,5.297,2.4232 +units=m +no_defs");
+      proj4.defs("EPSG:4326", "+proj=longlat +datum=WGS84 +no_defs");
+
+      const coordinates4326 = proj4("EPSG:31256", "EPSG:4326", responseData.features[0].geometry.coordinates);
+      // console.log(coordinates4326[1] + ',' + coordinates4326[0]);
+
+      return coordinates4326[1] + ',' + coordinates4326[0];
+  } catch (error) {
+      console.log('error'+error);
+  }
+
+}
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
