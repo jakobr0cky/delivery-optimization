c08c492a6a979e1c0a03010b234279a96a200b25
jakobr0cky <jakob.rcknbr@gmail.com>
2024-02-13 18:57:41 +0100

    changed ui and made current location in google maps link more accurate

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { default as proj4 } from 'proj4';
 import { CommonModule } from '@angular/common';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { Observable, catchError, map } from 'rxjs';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule, MatAutocompleteTrigger } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, MatSnackBarModule,HttpClientModule],
+  imports: [CommonModule, MatSnackBarModule, MatAutocompleteModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
@@ -16,35 +15,12 @@ export class MainComponent {
   input: string = '';
   options: string[] = [];
   googleMapsLink: string = '';
+  @ViewChild('trigger') inputField!: MatAutocompleteTrigger;
 
-  constructor(private snackBar: MatSnackBar,private http:HttpClient) {
-
-  }
-
-  ngAfterViewInit() {
-    //this.watchLocation();
-    if ("geolocation" in navigator) {
-      console.log('Geolocation API working');
-    } else {
-      this.snackBar.open('Browser does not support the Geolocation API!!', 'close', {
-        duration: 2000,
-        horizontalPosition: 'center',
-        verticalPosition: 'top',
-      });
-    }
-
-    
-  }
-
-  onItemClick(event: Event) {
-    var element = event.target as HTMLInputElement;
-    this.input = element.innerHTML;
-  }
+  constructor(private snackBar: MatSnackBar) { }
 
   async updateDropdown(event: Event) {
     var element = event.target as HTMLInputElement;
-
-    // this.useOpenStreetMap(element.value);
 
     var response = await fetch(`https://data.wien.gv.at/daten/OGDAddressService.svc/GetAddressInfo?Address=${element.value}&limit=10`);
 
@@ -67,36 +43,8 @@ export class MainComponent {
     }
   }
 
-  useOpenStreetMap(value:string){
-    this.getAddressesStartingWith(value).subscribe(
-      addresses => {
-        this.options = addresses;
-      },
-      error => {
-        console.error('Error:', error.message);
-      }
-    );
-  }
-
-  watchLocation() {
-    const geoId = navigator.geolocation.watchPosition(
-      (position) => {
-        const lat = position.coords.latitude;
-        const lng = position.coords.longitude;
-        console.log('watching: ');
-        console.log(`${lat},${lng}  ${position.coords.accuracy}`);
-        if (position.coords.accuracy > 10) {
-          console.log("The GPS accuracy isn't good enough");
-        }
-      },
-      (e) => {
-        console.log("ERROR: " + e.message);
-      },
-      { enableHighAccuracy: true, maximumAge: 2000, timeout: 5000 }
-    );
-  }
-
   createGoogleMapsLink(event?: Event) {
+    this.inputField.closePanel();
     this.googleMapsLink = '';
 
     var adress;
@@ -109,19 +57,7 @@ export class MainComponent {
     }
 
     this.getCoordinates(adress).then((result) => {
-      var location;
-      if (!("geolocation" in navigator)) {
-        location = 'standard location'
-      }
-      navigator.geolocation.getCurrentPosition((position) => {
-        const currentLocation = position.coords.latitude + ',' + position.coords.longitude;
-
-        this.googleMapsLink = `https://www.google.com/maps/dir/${currentLocation}/${result}`;
-      }, null, {
-        enableHighAccuracy: true, // Use high-accuracy methods if available
-        timeout: 10000, // Timeout in milliseconds
-        maximumAge: 0 // Force a fresh location even if cached
-      });
+      this.googleMapsLink = `https://www.google.com/maps/dir/?api=1&destination=${result}&travelmode=driving`;
     });
   }
 
@@ -140,20 +76,5 @@ export class MainComponent {
       console.log('error: ' + error);
       return;
     }
-  }
-  getAddressesStartingWith(prefix: string): Observable<string[]> {
-    const url = `https://nominatim.openstreetmap.org/search.php?q=${encodeURIComponent(prefix)}&format=json&addressdetails=1`;
-    return this.http.get<any[]>(url).pipe(
-      map(data => {
-        if (data && data.length > 0) {
-          return data.map(item => item.display_name);
-        } else {
-          throw new Error('No addresses found starting with the given prefix');
-        }
-      }),
-      catchError(error => {
-        throw new Error('Failed to retrieve addresses from OpenStreetMap API: ' + error.message);
-      })
-    );
   }
 }
